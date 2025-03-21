7c6ab3aa0bdc4ad48f9a5a5704c64ba40bf42622
Maria Laura Castro <cmarialaura599@gmail.com>
2025-03-21 12:59:10 -0300

    create day1.js
    Primeiro dia do desafio -> variáveis, if e else, == e === e tipagem

---
+++ day1.js
@@ -0,0 +1,31 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+// questão 01
+
+if (numeroUm == stringUm) { // correto
+    console.log('1) As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('1) As variáveis numeroUm e stringUm não tem o mesmo valor')
+} 
+
+// questão 02
+
+if (numeroTrinta === stringTrinta) { // correto
+    console.log('2) As variáveis numeroTrinta e stringTrinta tem o mesmo valor e o mesmo tipo')
+} else {
+    console.log('2) As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+// questão 03
+
+if (numeroDez == stringDez) { // correto
+    console.log('3) As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+    console.log('3) As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
+
